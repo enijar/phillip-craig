78822fea91c0804b647c6cfda8d6091bd1cc8795
Enijar <TheEnijar@gmail.com>
2019-02-11 20:04:42 +0000

    Added config and setup for JWT authentication

---
+++ app/PhillipCraig/Entities/User.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\DeBeers\Entities;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class User extends Authenticatable implements JWTSubject
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'active',
+        'code',
+    ];
+
+    protected $hidden = [
+        'password',
+    ];
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
+    /**
+     * The currently authenticated user
+     * @return null|User
+     */
+    public static function auth(): ?User
+    {
+        return auth()->user();
+    }
+}
--- config/app.php
+++ config/app.php
@@ -165,13 +165,13 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
