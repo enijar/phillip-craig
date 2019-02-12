c94af86234c65120abdb88542b2d2968b2bb6d94
James Craig <jamescraig@finervision.com>
2019-02-12 08:59:03 +0000

    Fixed CDN links

--- resources/js/Components/BackgroundVideo.js
+++ resources/js/Components/BackgroundVideo.js
@@ -5,9 +5,9 @@ export default function BackgroundVideo(props) {
     return (
         <div className="BackgroundVideo">
             <div className="BackgroundVideo__video">
-                <video muted autoPlay loop playsInline placeholder={cdn('vid/background-placeholder.jpg')}>
-                    <source src={cdn('vid/background.webm')} type="video/webm"/>
-                    <source src={cdn('vid/background.mp4')} type="video/mp4"/>
+                <video muted autoPlay loop playsInline placeholder={cdn('background-placeholder.jpg')}>
+                    <source src={cdn('background.webm')} type="video/webm"/>
+                    <source src={cdn('background.mp4')} type="video/mp4"/>
                 </video>
             </div>
 
