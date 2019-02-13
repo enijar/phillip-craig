41073daac229fe4927934c4a0b13b8127e7f5d77
Enijar <TheEnijar@gmail.com>
2019-02-13 20:43:35 +0000

    Formatting

--- app/PhillipCraig/Validators/ReCaptcha.php
+++ app/PhillipCraig/Validators/ReCaptcha.php
@@ -9,15 +9,12 @@ class ReCaptcha
     public function validate($attribute, $value, $parameters, $validator)
     {
         $client = new Client;
-        $response = $client->post('https://www.google.com/recaptcha/api/siteverify',
-            [
-                'form_params' =>
-                    [
-                        'secret' => env('RECAPTCHA_SECRET_KEY'),
-                        'response' => $value
-                    ]
-            ]
-        );
+        $response = $client->post('https://www.google.com/recaptcha/api/siteverify', [
+            'form_params' => [
+                'secret' => env('RECAPTCHA_SECRET_KEY'),
+                'response' => $value,
+            ],
+        ]);
         $body = json_decode((string)$response->getBody());
         return $body->success;
     }
