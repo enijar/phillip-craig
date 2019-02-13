3c7a0e7901ae4ccc12693c56829274fda60bce9e
Enijar <TheEnijar@gmail.com>
2019-02-13 20:03:42 +0000

    Loop

--- resources/js/Screens/SubscriptionScreen.js
+++ resources/js/Screens/SubscriptionScreen.js
@@ -15,10 +15,14 @@ const INITIAL_STATE = {
     },
 };
 
+const MAX_CAPTCHA_RESETS = 5;
+
 export default class SubscriptionScreen extends BaseScreen {
     state = JSON.parse(JSON.stringify(INITIAL_STATE));
 
     captcha = createRef();
+
+    captchaResets = 0;
 
     handleChange = event => {
         const {data} = this.state;
@@ -39,6 +43,15 @@ export default class SubscriptionScreen extends BaseScreen {
     };
 
     resetCaptcha = async () => {
+        console.log('resetCaptcha');
+
+        this.captchaResets++;
+
+        if (this.captchaResets === MAX_CAPTCHA_RESETS) {
+            this.captchaResets = MAX_CAPTCHA_RESETS;
+            return;
+        }
+
         await this.updateCaptcha();
 
         if (this.captcha.current) {
