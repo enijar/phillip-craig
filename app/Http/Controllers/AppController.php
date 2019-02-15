9693440ca319047df922d8e46ef519fe91aba596
James Craig <jamescraig@finervision.com>
2019-02-15 08:52:16 +0000

    Add redis for view caching

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Cache;
+
 class AppController extends Controller
 {
     public function app()
     {
-        return view('app');
+        return Cache::remember('view.app', 525600, function () {
+            return view('app')->render();
+        });
     }
 }
