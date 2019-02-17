0010ee25dd24dc0b298455735f34d42ec832e383
Enijar <TheEnijar@gmail.com>
2019-02-17 12:51:43 +0000

    Subscribe users to sendgrid email contacts

--- app/Http/Controllers/Api/SubscriptionController.php
+++ app/Http/Controllers/Api/SubscriptionController.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Notifications\NewSubscription;
 use App\PhillipCraig\Entities\Subscription;
 use App\PhillipCraig\Http\JsonResponse;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Notification;
+use SendGrid;
 
 class SubscriptionController extends Controller
 {
@@ -34,10 +36,23 @@ class SubscriptionController extends Controller
             return JsonResponse::errors(['Unable to subscribe, try again']);
         }
 
+        // @todo Move these external API calls to a queue to process. It's bad practice to
+        // make the user wait for these requests to finish
         if (env('APP_ENV') === 'production') {
             Notification::route('slack', env('SLACK_WEBHOOK_URL'))
                 ->notify(new NewSubscription($subscription));
         }
+
+        $sg = new SendGrid(env('SENDGRID_API_KEY'));
+
+        try {
+            $response = $sg->client->contactdb()->recipients()->post([request()->only(['email'])]);
+            die(var_dump($response->statusCode(), $response->body()));
+        } catch (\Exception $exception) {
+            Log::error($exception->getMessage());
+        }
+
+        // @todo Send verification of subscription to user's email
 
         return JsonResponse::message("You're subscribed");
     }
