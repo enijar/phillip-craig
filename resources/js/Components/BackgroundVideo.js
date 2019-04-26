1322a7d61afb672781858657dbcdc7c914afebfe
Enijar <TheEnijar@gmail.com>
2019-04-26 15:06:24 +0100

    Removed

--- resources/js/Components/BackgroundVideo.js
+++ resources/js/Components/BackgroundVideo.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import {cdn} from "../app/utils";
+import {asset} from "../app/utils";
 
 BackgroundVideo.propTypes = {
     mode: PropTypes.string,
@@ -14,9 +14,9 @@ export default function BackgroundVideo(props) {
     return (
         <div className={`BackgroundVideo BackgroundVideo--${props.mode}`}>
             <div className="BackgroundVideo__video">
-                <video muted autoPlay loop playsInline placeholder={cdn('background-placeholder.jpg')}>
-                    <source src={cdn('background.webm')} type="video/webm"/>
-                    <source src={cdn('background.mp4')} type="video/mp4"/>
+                <video muted autoPlay loop playsInline placeholder={asset('vid/background-placeholder.jpg')}>
+                    <source src={asset('vid/background.webm')} type="video/webm"/>
+                    <source src={asset('vid/background.mp4')} type="video/mp4"/>
                 </video>
             </div>
 
